7518f9e20b1da5b8552ea3e2cf452b333c5ead50
seiya9012 <seiya.ishizaki9012@gmail.com>
2021-09-25 01:25:43 +0900

    jissou2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,8 @@
 class PrototypesController < ApplicationController
+  #CSRF保護を無効にする
+  before_action :authenticate_user!, except: [:index, :show]
   def index
-    @prototypes = Prototype.includes(:prototype)
+    @prototypes = Prototype.all
   end
 
   def new
@@ -21,6 +23,8 @@ class PrototypesController < ApplicationController
 
   def show
     @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
   end
 
   def edit
@@ -28,9 +32,9 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = prototype.find(params[:id])
-    prototype.update(prototype_params)
-    if prototype.save
+    @prototype = Prototype.find(params[:id])
+    @prototype.update(prototype_params)
+    if @prototype.save
       redirect_to root_path(@prototype)
     else
       @prototype = @user.prototype.includes(:user)
@@ -48,6 +52,8 @@ class PrototypesController < ApplicationController
    def prototype_params
     params[:prototype].permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
    end
+
+
 
    def move_to_index
     unless user_signed_in?
